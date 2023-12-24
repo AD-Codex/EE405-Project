b29726ab4f5fb24d949b9fe32ad3f7daff8a47c2
VenuraShyamal14 <shamalbandara14@gmail.com>
2023-12-24 12:59:50 +0530

    added lane keep algorithm

--- computer vision/lane detection/lane_detect.py
+++ computer vision/lane detection/lane_detect.py
@@ -1,6 +1,68 @@
 import cv2
 from ultralytics import YOLO
 
+# lane keep variables
+
+import numpy as np
+import time
+
+wheel_linear_x = 0
+wheel_linear_z = 0
+Wheel_angular_z = 0
+
+Kp = 0.0001
+Ki = 0.00001
+Kd = 0.009
+
+P=0
+I=0
+D=0
+
+x=1
+y=10
+Vx=0
+Vy=10
+
+A=0
+B=0
+D=0
+E=10
+
+t=1
+m=100
+n=10
+
+image_lane = np.array([0,0,0,0,-1,-1,-1,-2,-2,-3])
+
+weight_for_lane = 10   
+
+vector = np.array([[x],[y],[Vx],[Vy]])
+
+predicted_vector = np.array([[0],[0],[0],[0]])
+
+actual_measurements = np.array([[0],[0],[0],[0]])
+
+PID_error = 1
+pre_PID_error = 0
+
+image_error = 0
+
+IMU = 0
+pre_IMU = 0
+
+F = np.array([[1,0,t,0],[0,1,0,0],[B,0,A,0],[0,E,0,D]])
+H = np.array([[1,0,t,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]])
+
+PCM = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+predicted_PCM = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+
+ProcessNoice_forPCM = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+ProcessNoice_forPredictedVector = np.array([[0],[0],[0],[0]])
+measurementNoice = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+
+end_time = 0
+
+# end of lane keep variables
 
 
 def floor_to_nearest_5(number):
@@ -90,9 +152,71 @@ while cap.isOpened():
         # change the sorting method in line 77 in order to sort from bottom of the screen to top
         displacement_list = [dispacement_dic[key] for key in dispacement_dic]
 
-        print(length_of_displacement_list)
-        print("displacement ",dispacement_dic)
-        print("displacement list",displacement_list)
+        # print(length_of_displacement_list)
+        # print("displacement ",dispacement_dic)
+        # print("displacement list",displacement_list)
+        n= len(displacement_list)
+        image_lane=displacement_list
+
+        # lane keep algorithm
+        for i in range (m) :
+
+            strt_time = time.time()
+
+            t = (strt_time - end_time)/1000000000
+
+            F[0][2] = t
+            H[0][2] = t
+            # print(t)
+
+            image_error = 0
+
+            for j in range (n):
+                image_error = image_error + image_lane[j]*weight_for_lane/(j+1) #weighted sum from the image data
+
+            # Get IMU readings here 
+
+            # print(image_error)
+
+            PID_error = PID_error - (pre_IMU - IMU) + image_error
+
+            # print(PID_error)
+
+            # if PID_error < 0 :
+            #     PID_error = 0
+
+            P = PID_error
+            I = I + PID_error
+            D = PID_error - pre_PID_error
+
+            F[2,0] = P*Kp + I*Ki + D*Kd
+            # print(F[2,0])
+
+            # print(vector[2])
+
+            actual_measurements[0] = image_lane[0] ##check
+            actual_measurements[1] = vector[1]
+            actual_measurements[2] = vector[2]
+            actual_measurements[3] = vector[3]
+
+
+            predicted_vector = np.matmul(F,vector) + ProcessNoice_forPredictedVector
+            predicted_PCM = np.matmul(F, np.matmul(PCM,F.transpose())) + ProcessNoice_forPCM
+
+            # print(vector[0])
+
+            vector = predicted_vector + np.matmul(np.matmul(predicted_PCM, np.matmul(H.transpose(), (np.matmul(H, np.matmul(predicted_PCM, H.transpose())) + measurementNoice))), (actual_measurements - np.matmul(H,predicted_vector)))
+
+            PCM = np.matmul((np.identity(4) - np.matmul(np.matmul(predicted_PCM, np.matmul(H.transpose(), (np.matmul(H, np.matmul(predicted_PCM, H.transpose())) + measurementNoice))), H)), predicted_PCM)
+
+            print(vector.transpose())
+
+            # vector[0] = image_lane[0]
+
+
+
+            # end of lane keep algorithm
+
 
 
         # add any thing here
